74432f327cf4b6adea1b71a311a6fcdec019c8eb
dlswns2480 <dlswns2480@naver.com>
2023-11-17 01:23:46 +0900

    페치 조인 EntityGraph 활용

--- jpa-lab/src/main/java/jpa/laboratory/jpalab/repository/OrderRepository.java
+++ jpa-lab/src/main/java/jpa/laboratory/jpalab/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package jpa.laboratory.jpalab.repository;
 
+import jakarta.persistence.Entity;
 import jpa.laboratory.jpalab.domain.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,6 +11,7 @@ import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Query("select o from Order o join fetch o.member m")
+//    @Query("select o from Order o join fetch o.member m")
+    @EntityGraph(attributePaths = "member")
     List<Order> findAll();
 }
