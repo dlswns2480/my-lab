1a44a466682a09fb5fd8a14dd1bdec0784ed844d
dlswns2480 <dlswns2480@naver.com>
2023-12-13 22:42:52 +0900

    ID strategy 변경

--- jpa-lab/src/main/java/jpa/laboratory/jpalab/domain/Member.java
+++ jpa-lab/src/main/java/jpa/laboratory/jpalab/domain/Member.java
@@ -1,40 +1,36 @@
 package jpa.laboratory.jpalab.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Entity
-@Getter @Setter
+@Getter
 @NoArgsConstructor
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "member_id")
     private Long id;
-    private String dummys;
-
     private String name;
-
     private String email;
-    private int postcode;
-
-    @JsonIgnore
-    @OneToMany(mappedBy = "member")
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Order> order = new ArrayList<>();
 
     @Builder
-    public Member(String name, String email, int postcode){
+    public Member(String name, String email){
         this.name = name;
         this.email = email;
-        this.postcode = postcode;
     }
-
 }
